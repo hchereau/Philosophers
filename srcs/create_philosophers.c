50b99ba9f2d53604b8b951afd390b3eff029ff53
hchereau <115716283+hchereau@users.noreply.github.com>
2025-07-05 10:35:31 +0200

    Improve memory handling and timing

--- srcs/create_philosophers.c
+++ srcs/create_philosophers.c
@@ -17,6 +17,16 @@ void	*philosopher_routine(void *arg)
 	t_philosopher	*philosopher;
 
 	philosopher = (t_philosopher *)arg;
+       if (philosopher->data->num_philosophers == 1)
+       {
+               pthread_mutex_lock(philosopher->left_forks);
+               print_status(philosopher, FORKS_TAKEN);
+               usleep(philosopher->data->time_to_die * 1000);
+               pthread_mutex_unlock(philosopher->left_forks);
+               return (NULL);
+       }
+       if (philosopher->id % 2 == 1)
+               usleep(philosopher->data->time_to_eat * 1000);
 	while (is_simulation_running(philosopher->data))
 	{
 		philosopher_think(philosopher);
@@ -32,15 +42,15 @@ t_state_data	create_philosophers(t_data *data, t_philosopher **philos)
 {
 	int	i;
 
-	*philos = malloc(sizeof(t_philosopher) * data->num_philosophers);
-	if (*philos == NULL)
-	{
-		printf("Error: Memory allocation failed for philosophers.\n");
-		return (FAILED_VALUE);
-	}
-	i = 0;
-	while (i < data->num_philosophers)
-	{
+        *philos = malloc(sizeof(t_philosopher) * data->num_philosophers);
+        if (*philos == NULL)
+        {
+                printf("Error: Memory allocation failed for philosophers.\n");
+                return (FAILED_VALUE);
+        }
+        i = 0;
+        while (i < data->num_philosophers)
+        {
 		(*philos)[i].id = i + 1;
 		(*philos)[i].state = THINKING;
 		(*philos)[i].meals_eaten = 0;
@@ -48,12 +58,19 @@ t_state_data	create_philosophers(t_data *data, t_philosopher **philos)
 		(*philos)[i].right_forks = &data->forks[(i + 1) % data->num_philosophers];
 		(*philos)[i].data = data;
 		(*philos)[i].last_meal_time = data->start_time;
-		if (pthread_create(&(*philos)[i].thread, NULL, philosopher_routine, &(*philos)[i]) != 0)
-		{
-			printf("Error: Failed to create philosopher thread %d.\n", i + 1);
-			return (FAILED_VALUE);
-		}
-		i++;
-	}
-	return (SUCCESS_VALUE);
+                if (pthread_create(&(*philos)[i].thread, NULL, philosopher_routine, &(*philos)[i]) != 0)
+                {
+                        printf("Error: Failed to create philosopher thread %d.\n", i + 1);
+                        pthread_mutex_lock(&data->simulation_mutex);
+                        data->simulation_running = 0;
+                        pthread_mutex_unlock(&data->simulation_mutex);
+                        while (i-- > 0)
+                                pthread_join((*philos)[i].thread, NULL);
+                        free(*philos);
+                        *philos = NULL;
+                        return (FAILED_VALUE);
+                }
+                i++;
+        }
+        return (SUCCESS_VALUE);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -25,25 +25,50 @@ void	*monitor_routine(void *arg)
 	while (is_simulation_running(data))
 	{
 		i = 0;
-		while (i < data->num_philosophers)
-		{
-			now = get_timestamp();
-			pthread_mutex_lock(&data->simulation_mutex);
-			last_meal_time = philos[i].last_meal_time;
-			pthread_mutex_unlock(&data->simulation_mutex);
-			if (now - last_meal_time > data->time_to_die)
-			{
-				print_status(&philos[i], DEAD);
-				pthread_mutex_lock(&data->simulation_mutex);
-				data->simulation_running = 0;
-				pthread_mutex_unlock(&data->simulation_mutex);
-				return (NULL);
-			}
-			i++;
-		}
-		usleep(1000);
-	}
-	return (NULL);
+                while (i < data->num_philosophers)
+                {
+                        now = get_timestamp();
+                        pthread_mutex_lock(&data->simulation_mutex);
+                        last_meal_time = philos[i].last_meal_time;
+                        pthread_mutex_unlock(&data->simulation_mutex);
+                        if (now - last_meal_time > data->time_to_die)
+                        {
+                                print_status(&philos[i], DEAD);
+                                pthread_mutex_lock(&data->simulation_mutex);
+                                data->simulation_running = 0;
+                                pthread_mutex_unlock(&data->simulation_mutex);
+                                return (NULL);
+                        }
+                        i++;
+                }
+               if (data->max_meals != INFINITE_MEALS)
+               {
+                       int     j;
+                       int     all_full;
+
+                       all_full = 1;
+                       j = 0;
+                       while (j < data->num_philosophers)
+                       {
+                               pthread_mutex_lock(&data->simulation_mutex);
+                               if (philos[j].meals_eaten < data->max_meals)
+                                       all_full = 0;
+                               pthread_mutex_unlock(&data->simulation_mutex);
+                               if (!all_full)
+                                       break ;
+                               j++;
+                       }
+                       if (all_full)
+                       {
+                               pthread_mutex_lock(&data->simulation_mutex);
+                               data->simulation_running = 0;
+                               pthread_mutex_unlock(&data->simulation_mutex);
+                               return (NULL);
+                       }
+               }
+                usleep(1000);
+        }
+        return (NULL);
 }
 
 static void	start_simulation(t_data *data, t_philosopher *philos)
@@ -69,19 +94,30 @@ int	main(int argc, char **argv)
 		printf("Usage: %s num_philosophers time_to_die time_to_eat time_to_sleep [max_meals]\n", argv[0]);
 		return (FAILURE);
 	}
-	if (init_data(&data, argc, argv) != SUCCESS)
-	{
-		return (FAILURE);
-	}
+       philos = NULL;
+       if (init_data(&data, argc, argv) != SUCCESS_VALUE)
+        {
+                return (FAILURE);
+        }
 	data.start_time = get_timestamp();
-	if (create_philosophers(&data, &philos) != SUCCESS)
-	{
-		printf("Error: Failed to create philosophers.\n");
-		return (FAILURE);
-	}
-	pthread_create(&data.monitor_thread, NULL, monitor_routine, philos);
-	start_simulation(&data, philos);
-	pthread_join(data.monitor_thread, NULL);
-	cleanup(&data, philos);
+       if (create_philosophers(&data, &philos) != SUCCESS_VALUE)
+        {
+                printf("Error: Failed to create philosophers.\n");
+                cleanup(&data, NULL);
+                return (FAILURE);
+        }
+        if (pthread_create(&data.monitor_thread, NULL, monitor_routine, philos) != 0)
+        {
+                printf("Error: Failed to create monitor thread.\n");
+                pthread_mutex_lock(&data.simulation_mutex);
+                data.simulation_running = 0;
+                pthread_mutex_unlock(&data.simulation_mutex);
+                start_simulation(&data, philos);
+                cleanup(&data, philos);
+                return (FAILURE);
+        }
+        start_simulation(&data, philos);
+        pthread_join(data.monitor_thread, NULL);
+        cleanup(&data, philos);
 	return (SUCCESS);
 }
